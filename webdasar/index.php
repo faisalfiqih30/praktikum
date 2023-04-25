a516a3dc3ab0565b1bd5dab3c60b23871a1189a0
kamiliiaa <nadayasin106@yahoo.co.id>
2023-04-25 23:18:58 +0700

    tambah php

---
+++ webdasar/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    $nama_kamu = "Fiqih";
+    $umur = 21;
+
+    //ini untuk komentar
+    /*
+    echo " hello word, welcome ".$nama_kamu; 
+    echo " umur kamu ".$umur;
+    echo " tahun ";
+    echo " <br>"
+    */
+
+    echo "hello word, welcome ".$nama_kamu. " umur kamu ".$umur. "tahun";
+    echo "<br>";
+    echo "berarti $nama_kamu lahit di tahun";
+    echo 2023-$umur
+
+    //selesai
+
+?>
